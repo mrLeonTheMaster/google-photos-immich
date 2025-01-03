5b23b5ba4b4c3d999bad8c736f55aa6d4f335314
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2025-01-03 18:58:41 +0100

    Update uploader.sh

--- linux/uploader.sh
+++ linux/uploader.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+read -p "Enter the URL of the Immich server (including http:// or https://): " server_url
+read -p "Enter an API key: " api_key
+
+echo "Downloading the immich-go utility..."
+wget -q --show-progress "https://github.com/mrLeonTheMaster/google-photos-immich/raw/refs/heads/main/linux/immich-go"
+
+./immich-go -server=%server_url% -key=%api_key% upload -create-albums -google-photos takeout-*.zip
