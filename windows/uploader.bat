79ccccb5705bf4f3e42a5ab8c0fb14ca41c6193d
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2025-01-03 17:31:32 +0100

    Update uploader.bat

--- windows/uploader.bat
+++ windows/uploader.bat
@@ -3,6 +3,7 @@
 set /p server_url=Enter the URL of the Immich server (including http:// or https://): 
 set /p api_key=Enter an API key: 
 
-powershell -Command '$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest "https://github.com/mrLeonTheMaster/google-photos-immich/raw/refs/heads/main/windows/immich-go.exe" -OutFile immich-go.exe'
+echo Downloading the immich-go utility...
+powershell -Command $ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest "https://github.com/mrLeonTheMaster/google-photos-immich/raw/refs/heads/main/windows/immich-go.exe" -OutFile immich-go.exe
 
 .\immich-go -server=%server_url% -key=%api_key% upload -create-albums -google-photos takeout-*.zip
