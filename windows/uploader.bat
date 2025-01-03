e331dca92ebb9f8db5ed682600cf48dbb7cced2d
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2025-01-03 17:49:18 +0100

    Update uploader.bat

--- windows/uploader.bat
+++ windows/uploader.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "text=Immich Uploader" & title %text% & set /a width=80, len=0, pad=(width-(for /l %%A in (0,1,1024) do if not "!text:~%%A,1!"=="" set /a len+=1)) / 2 & set "line=" & for /l %%A in (1,1,%pad%) do set "line=!line!-!" & set "line=!line!!text!" & for /l %%A in (1,1,%pad%) do set "line=!line!-!" & echo !line:~0,%width%!
+set "text=Immich Uploader" & title %text% & setlocal enabledelayedexpansion & set /a width=80, len=0 & for /l %%A in (0,1,1024) do if not "!text:~%%A,1!"=="" set /a len+=1 & set /a pad=(width-len)/2 & if %pad% LSS 0 set pad=0 & set "line=" & for /l %%A in (1,1,%pad%) do set "line=!line!-!" & set "line=!line!!text!" & for /l %%A in (1,1,%pad%) do set "line=!line!-!" & echo !line:~0,%width%!
 
 set /p server_url=Enter the URL of the Immich server (including http:// or https://): 
 set /p api_key=Enter an API key: 
