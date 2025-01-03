bb7f7bad92ec57e5c063673280dbcd6a6f192b5d
mrLeonTheMaster <78276856+mrLeonTheMaster@users.noreply.github.com>
2025-01-03 17:44:22 +0100

    Update uploader.bat

--- windows/uploader.bat
+++ windows/uploader.bat
@@ -1,7 +1,7 @@
 @echo off
 
 title Immich Uploader
-echo "---------- Immich Uploader ----------"
+set "text=Immich Uploader" & set /a width=80, len=0, pad=(width-(for /l %%A in (0,1,1024) do if not "!text:~%%A,1!"=="" set /a len+=1)) / 2 & set "line=" & for /l %%A in (1,1,%pad%) do set "line=!line!-!" & set "line=!line!!text!" & for /l %%A in (1,1,%pad%) do set "line=!line!-!" & echo !line:~0,%width%!
 
 set /p server_url=Enter the URL of the Immich server (including http:// or https://): 
 set /p api_key=Enter an API key: 
